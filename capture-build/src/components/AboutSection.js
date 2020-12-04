4c7551398f38682db081b1b56199ddb205fe0bb1
Jortega23 <jortegs23@gmail.com>
2020-12-03 21:44:50 -0800

    Styling WIP
    Styling components WIP

--- capture-build/src/components/AboutSection.js
+++ capture-build/src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import home1 from '../images/home1.png'
+//Styling
+import styled from 'styled-components'
 
 const AboutSection = () => {
   return (
-    <div>
+    <About>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -22,8 +24,18 @@ const AboutSection = () => {
           <img src={home1} alt='guy with camera'/>
         </div>
       </div>
-    </div>
+    </About>
   )
-}
+};
+
+//Styled component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`
 
 export default AboutSection
